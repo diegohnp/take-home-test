352b697bca515c6bbaacb3603b9df15c3c913a73
Diego Henrique <digohnp@gmail.com>
2024-01-31 18:40:47 -0300

    feat: EventController class created to separate endpoint

---
+++ src/main/kotlin/com/digohnp/ebanxtest/controller/EventController.kt
@@ -0,0 +1,9 @@
+package com.digohnp.ebanxtest.controller
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/event")
+class EventController {
+}
