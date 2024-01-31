f4fb77f2552f325692cb73f3d863ad351ac06fa4
Diego Henrique <digohnp@gmail.com>
2024-01-31 19:55:50 -0300

    upd: balanceService service injected on EventService

--- src/main/kotlin/com/digohnp/ebanxtest/service/EventService.kt
+++ src/main/kotlin/com/digohnp/ebanxtest/service/EventService.kt
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Service
 
 @Service
 class EventService {
+class EventService(
+    val balanceService: BalanceService
+) {
 
     private fun canExecuteEvent(request: PostBalanceRequest): Boolean {
         return "deposit" == request.type.lowercase(Locale.getDefault())
