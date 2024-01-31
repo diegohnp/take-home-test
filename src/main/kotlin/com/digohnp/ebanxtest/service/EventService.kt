a142c2b361ae88582551523de4a6c467a35e8dc1
Diego Henrique <digohnp@gmail.com>
2024-01-31 19:57:12 -0300

    upd: sendEvent method copy from BalanceService to EventService and calls methods from balance service

--- src/main/kotlin/com/digohnp/ebanxtest/service/EventService.kt
+++ src/main/kotlin/com/digohnp/ebanxtest/service/EventService.kt
@@ -1,12 +1,25 @@
 package com.digohnp.ebanxtest.service
 
+import com.digohnp.ebanxtest.controller.request.PostBalanceRequest
+import com.digohnp.ebanxtest.controller.response.PostEventResponse
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
-class EventService {
 class EventService(
     val balanceService: BalanceService
 ) {
+
+    fun sendEvent(request: PostBalanceRequest): PostEventResponse? {
+        if (!canExecuteEvent(request)) return null
+
+        return when (request.type.lowercase(Locale.getDefault())) {
+            "deposit" -> balanceService.deposit(request)
+            "withdraw" -> balanceService.withdraw(request)
+            "transfer" -> balanceService.transfer(request)
+            else -> null
+        }
+    }
 
     private fun canExecuteEvent(request: PostBalanceRequest): Boolean {
         return "deposit" == request.type.lowercase(Locale.getDefault())
